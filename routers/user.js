feffb38341b8e09b1816a356ff31d1c83e85a9b9
hightech0827 <hightech0827@gmail.com>
2024-05-24 10:18:53 +1000

    feat: add url get

--- routers/user.js
+++ routers/user.js
@@ -4,5 +4,8 @@ import userController from "../controllers/user.js";
 const router = express.Router();
 
 router.post('/location', userController);
+router.get('/location', (req, res)=>{
+    res.send("Hello world");
+});
 
 export default router;
